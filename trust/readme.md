1845c43bb759791f7100a44824db41d0934a20f3
teng <wwwsousoucom@126.com>
2020-03-30 20:15:56 +0800

    loop

--- trust/readme.md
+++ trust/readme.md
@@ -1,13 +1,15 @@
 # tcp
 
     sudo setcap cap_net_admin=eip /home/teng/CLionProjects/welcome/trust/target/debug/trust
-    
 # 添加ip地址
 
      sudo ip addr add 10.0.0.1/24 dev tun0
-     
 # 启动网卡
     
      sudo ip link set up dev tun0
+# 加入循环
+
+    修改run.sh
+# ping 10.0.0.2
     
-    
+    ping -I tun0 10.0.1.2
--- trust/src/main.rs
+++ trust/src/main.rs
@@ -2,7 +2,9 @@ use std::io;
 fn main() -> io::Result<()>{
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    let n_bytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x?}", n_bytes, &buf[..n_bytes]);
+    loop {
+        let n_bytes = nic.recv(&mut buf[..])?;
+        eprintln!("read {} bytes: {:x?}", n_bytes, &buf[..n_bytes]);
+    }
     Ok(())
 }
