c1bb150b5a1eb1b180bd7885058d774e069b6955
teng <wwwsousoucom@126.com>
2020-03-30 21:23:51 +0800

    web server

---
+++ web_stream/src/main.rs
@@ -0,0 +1,28 @@
+use std::net::{TcpStream, TcpListener};
+use std::io::{Read, Write};
+use std::fs;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "template/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "template/404.html")
+    };
+    let content = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, content);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    for stream in listener.incoming() {
+        handle_client(stream?)
+    }
+    Ok(())
+} // the stream is closed here
