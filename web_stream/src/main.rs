e1c9361c5da7c676902f6997274a4ef370b5c77b
teng <wwwsousoucom@126.com>
2020-03-30 21:39:24 +0800

    多线程

--- web_stream/src/main.rs
+++ web_stream/src/main.rs
@@ -1,6 +1,6 @@
 use std::net::{TcpStream, TcpListener};
 use std::io::{Read, Write};
-use std::fs;
+use std::{fs, time, thread};
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -17,12 +17,24 @@ fn handle_client(mut stream: TcpStream) {
     let response = format!("{}{}", status_line, content);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+    let ten_millis = time::Duration::from_millis(10000);
+    thread::sleep(ten_millis);
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
     for stream in listener.incoming() {
-        handle_client(stream?)
+        // handle_client(stream?);
+        let stream = stream.unwrap();
+        let handle = thread::spawn(move||{
+           handle_client(stream);
+        });
+        thread_vec.push(handle);
+    }
+    for handle in thread_vec {
+        // handle.join().unwrap();
+        handle.join().unwrap();
     }
     Ok(())
 } // the stream is closed here
