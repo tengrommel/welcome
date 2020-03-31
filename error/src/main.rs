8eda291721e0dec3e1f3f6ff4c5d34cd78b3b24c
teng <wwwsousoucom@126.com>
2020-03-31 23:46:07 +0800

    ?

--- error/src/main.rs
+++ error/src/main.rs
@@ -23,3 +23,10 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Err(error) => Err(error),
     }
 }
+
+fn username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
