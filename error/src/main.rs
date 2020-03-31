f31e684d367290fb6971f7b15e5816d86092d68a
teng <wwwsousoucom@126.com>
2020-03-31 23:49:35 +0800

    简写

--- error/src/main.rs
+++ error/src/main.rs
@@ -30,3 +30,9 @@ fn username_from_file() -> Result<String, io::Error> {
     f.read_to_string(&mut s)?;
     Ok(s)
 }
+//
+fn simple() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
