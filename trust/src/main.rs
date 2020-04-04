f6a06df1e2b80e54d50ad679381233fc60de5e74
teng <wwwsousoucom@126.com>
2020-04-05 00:43:25 +0800

    proto

--- trust/src/main.rs
+++ trust/src/main.rs
@@ -1,10 +1,17 @@
 use std::io;
-fn main() -> io::Result<()>{
+
+fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
         let n_bytes = nic.recv(&mut buf[..])?;
-        eprintln!("read {} bytes: {:x?}", n_bytes, &buf[..n_bytes]);
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        eprintln!("read {} bytes(flags: {:x}, proto: {:x}): {:x?}",
+                  n_bytes - 4,
+                  flags,
+                  proto,
+                  &buf[..n_bytes]);
     }
     Ok(())
 }
