89ac8fde01d1bcf775e46d7bc988e73d1dc3bda1
teng <wwwsousoucom@126.com>
2020-04-05 14:35:56 +0800

    ping is over

--- trust/src/main.rs
+++ trust/src/main.rs
@@ -5,13 +5,33 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let n_bytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        eprintln!("read {} bytes(flags: {:x}, proto: {:x}): {:x?}",
-                  n_bytes - 4,
-                  flags,
-                  proto,
-                  &buf[..n_bytes]);
+        // let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..n_bytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                eprintln!("{} -> {} {}b of protocol {}",
+                          src,
+                          dst,
+                          proto,
+                          p.payload_len());
+            }
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e)
+            }
+        }
+
+        // eprintln!("read {} bytes(flags: {:x}, proto: {:x}): {:x?}",
+        //           n_bytes - 4,
+        //           eth_flags,
+        //           eth_proto,
+        //           &buf[..n_bytes]);
     }
     Ok(())
 }
