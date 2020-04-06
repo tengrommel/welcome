6ddaa0da1766001de6267b76fe13b16c9ef03501
teng <wwwsousoucom@126.com>
2020-04-06 11:34:13 +0800

    平均数

--- docker/rand_ex/src/main.rs
+++ docker/rand_ex/src/main.rs
@@ -6,8 +6,19 @@ fn main() {
     let length_nums = rng.gen_range(5, 25);
     println!("length of nums: {}", length_nums);
     let mut nums: Vec<usize> = Vec::new();
-    for i in 0..length_nums {
+    for _ in 0..length_nums {
         nums.push(rng.gen_range(0, 10));
     }
     println!("nums: {:?}", nums);
+    compute_mean(&mut nums);
 }
+
+fn compute_mean(n: &Vec<usize>) {
+    let mut sum: usize = 0;
+    for i in n {
+        sum += i; // 自动解引用
+    }
+    let median = sum/n.len();
+    print!("median: {}", median);
+}
+
