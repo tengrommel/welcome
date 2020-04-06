6007f274c7311ce3ac032b53e7dc8c401d23bd28
teng <wwwsousoucom@126.com>
2020-04-06 15:26:39 +0800

    中位数

--- docker/rand_ex/src/main.rs
+++ docker/rand_ex/src/main.rs
@@ -11,6 +11,8 @@ fn main() {
     }
     println!("nums: {:?}", nums);
     compute_mean(&mut nums);
+    compute_median(&mut nums);
+    // println!("num: {:?}", num);
 }
 
 fn compute_mean(n: &Vec<usize>) {
@@ -18,7 +20,31 @@ fn compute_mean(n: &Vec<usize>) {
     for i in n {
         sum += i; // 自动解引用
     }
-    let median = sum/n.len();
+    let median = sum / n.len();
     print!("median: {}", median);
+}
+
+fn bubble_sort_fn(nums: &mut Vec<usize>) {
+    for i in 0..nums.len() {
+        for j in ((i + 1)..nums.len()).rev() {
+            if nums[j - 1] > nums[j] {
+                swap_us(nums, j - 1, j);
+            }
+        }
+    }
+    println!("{:?}", nums);
+}
+
+fn swap_us(m_list: &mut Vec<usize>, i: usize, j: usize) {
+    let temp: usize;
+    temp = m_list[i];
+    m_list[i] = m_list[j];
+    m_list[j] = temp;
+}
+
+fn compute_median(n: &mut Vec<usize>) {
+    bubble_sort_fn(n);
+    let middle_pos = n.len()/2;
+    println!("median: {}", n.get(middle_pos).unwrap());
 }
 
