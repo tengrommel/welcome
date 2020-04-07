35ca8a1b0caa1ae010115d0b8e34c1ee0b833a35
teng <wwwsousoucom@126.com>
2020-04-07 21:46:57 +0800

    Rc智能指针计数

--- docker/ex/src/main.rs
+++ docker/ex/src/main.rs
@@ -16,7 +16,16 @@ fn main() {
     // let b = Cons(3, Box::new(a));
     // let c = Cons(4, Box::new(a));// move发生后还在使用
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    eprintln!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    eprintln!("count after bind to b, a count = {}", Rc::strong_count(&a));
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        eprintln!("count after bind to b, a count = {}", Rc::strong_count(&a));
+    }
+    eprintln!("count after bind to b, a count = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // let c = Cons(4, Rc::clone(&a));
     eprintln!("over")
 }
