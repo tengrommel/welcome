154488a842c090f5fe1e6a3515531c5a43aee562
teng <wwwsousoucom@126.com>
2020-04-07 22:07:14 +0800

    refcell

--- docker/micro/main/src/main.rs
+++ docker/micro/main/src/main.rs
@@ -1,3 +1,21 @@
+// 1、内部可变性：允许在使用不可变引用时改变数据
+// 2、通过RefCell<T>在运行时检查借用规则（通常情况下，是在编译时检查借用规则），
+// RefCell<T>代表其数据的唯一所有权
+use std::rc::Rc;
+use List::{Cons, Nil};
+use std::cell::RefCell;
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil
+}
+
 fn main() {
-    println!("Hello, world!");
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(7)), Rc::clone(&a));
+    *value.borrow_mut() += 10;
+    println!("a after: {:?}", a);
 }
