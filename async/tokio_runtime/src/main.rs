cc66698bd89483033d1596a9668aad8cf806ba86
teng <wwwsousoucom@126.com>
2020-04-09 12:06:35 +0800

    ok

--- async/tokio_runtime/src/main.rs
+++ async/tokio_runtime/src/main.rs
@@ -1,27 +1,32 @@
-use std::thread;
-use std::thread::JoinHandle;
-use std::time::Duration;
-use rand::prelude::*;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use futures::stream::StreamExt;
 
-fn main() {
-    // Spawn 100 tasks on threads
-    let thread_handles: Vec<JoinHandle<_>> =
-        (1..=100).map(|i|{
-            // 1-2 second delay
-            let delay = rand::thread_rng().gen_range(1000, 2000);
-            // Give this thread a unique name
-            let builder = thread::Builder::new().name(format!("custom-{}", i));
-            // Spawn it
-            builder.spawn(move || {
-                // Step 1: delay
-                thread::sleep(Duration::from_millis(delay));
-                // Step 2: print to screen
-                println!("Delay {} ms done! Thread name: {}", delay, thread::current().name().unwrap());
-            }).unwrap()
-        }).collect();
-    // Wait for all of them to finish
-    for h in thread_handles {
-        let _ = h.join();
-    }
-    println!("Job done");
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:6142";
+    let mut listener = TcpListener::bind(addr).await.unwrap();
+
+    // Here we convert the `TcpListener` to a stream of incoming connections
+    // with the `incoming` method.
+    let server = async move {
+        let mut incoming = listener.incoming();
+        while let Some(socket_res) = incoming.next().await {
+            match socket_res {
+                Ok(socket) => {
+                    println!("Accepted connection from {:?}", socket.peer_addr());
+                    // TODO: Process socket
+                }
+                Err(err) => {
+                    // Handle error by printing to STDOUT.
+                    println!("accept error = {:?}", err);
+                }
+            }
+        }
+    };
+
+    println!("Server running on localhost:6142");
+
+    // Start the server and block this async fn until `server` spins down.
+    server.await;
 }
