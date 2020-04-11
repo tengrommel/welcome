21a636a11404f184af13cfe95640ce4695dd07df
teng <wwwsousoucom@126.com>
2020-04-11 21:50:42 +0800

    例子有问题

--- ml/data_structures/hello_rust/src/main.rs
+++ ml/data_structures/hello_rust/src/main.rs
@@ -1,3 +1,13 @@
+use std::thread;
+
+fn threading() {
+    let x = 0;
+    let handle = thread::spawn(move ||{
+        println!("Hello from a thread, the number is: {}", x);
+    });
+    handle.join().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    threading();
 }
