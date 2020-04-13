8cb2b2fbb42adb65890d3b5c5c45d3bac48efba1
teng <wwwsousoucom@126.com>
2020-04-13 19:52:24 +0800

    读写文件

--- fit/src/main.rs
+++ fit/src/main.rs
@@ -1,3 +1,14 @@
+use std::path::Path;
+use std::fs::File;
+use std::error::Error;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let data_path = Path::new("data/HRSz0yd020fm_c2f.fits");
+    let mut file = match File::open(data_path) {
+        Err(e) => panic!("Couldn't open {}: {}", data_path.display(), e.description()),
+        Ok(file) => file,
+    };
+    let mut fits_data:Vec<u8> = Vec::new();
+    file.read_to_end(&mut fits_data);
 }
