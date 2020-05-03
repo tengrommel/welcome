5760e2c561b1a198f8fc29827b922989bd7ffeb3
teng <wwwsousoucom@126.com>
2020-05-03 20:06:57 +0800

    structopt

--- command_line/src/main.rs
+++ command_line/src/main.rs
@@ -4,11 +4,14 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Options {
     message: String, // you define a struct named Options that has one String field called message. Then you annotate the struct with the custom derive attribute #[derive(StructOpt)]. This way StructOpt
+    #[structopt(short = "d", long = "dead")]
+    dead: bool,
 }
 
 fn main() {
     let options = Options::from_args();
     let message = options.message;
+    let eye = if options.dead{"x"} else {"o"};
     println!("{}", message);
     println!(" \\");
     println!(" \\");
