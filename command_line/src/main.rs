872ece327beba8e4140fe1a99d87dcabed695727
teng <wwwsousoucom@126.com>
2020-05-03 20:15:46 +0800

    err std

--- command_line/src/main.rs
+++ command_line/src/main.rs
@@ -8,11 +8,22 @@ struct Options {
     dead: bool,
 }
 
+// Printing to STDERR
+/**
+Up until now, you have been printing using println!(), which print to
+the standard output(STDOUT). However, there is also the standard error (STDERR)
+stream that you can and should print errors to.
+Rust provides a STDERR equivalent of println!(), called eprintln!().
+*/
+
 fn main() {
     let options = Options::from_args();
     let message = options.message;
     let eye = if options.dead{"x"} else {"o"};
-    println!("{}", message);
+    println!("{}", message); // get the message
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog.")
+    }
     println!(" \\");
     println!(" \\");
     println!(" /\\_/\\");
